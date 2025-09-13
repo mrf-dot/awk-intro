3d4b518aa1edd053017eb404bde90c4f544ce807
Mitch Feigenbaum <mitch@mitchf.me>
2025-09-13 09:29:57 -0400

    change

--- python_readfile_example.py
+++ python_readfile_example.py
@@ -3,5 +3,5 @@ import re
 with open('file.txt', 'r') as f:
     for line in f:
         cols = line.split()
-        if re.search('Ripley', cols[0]):
+        if re.search('Mike', cols[0]):
             print(cols[1])
